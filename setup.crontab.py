39f9cd5acc60edfdc8701054a1ac1037a8342c9c
rudda <beltrao.rudah@gmail.com>
2022-03-05 15:20:49 -0400

    style: code style changes - breaking lines

--- setup.crontab.py
+++ setup.crontab.py
@@ -1,7 +1,11 @@
 import os
 
 def job():
-    job_txt = os.environ['TASK_SCHEDULE']+" "+ os.environ['USER_NAME']+ " "+ os.environ['COMMAND'] + " >/proc/1/fd/1 2>/proc/1/fd/2" 
+    job_txt = os.environ['TASK_SCHEDULE']+" "+ \
+              os.environ['USER_NAME']+ " "+ \
+              os.environ['COMMAND'] + \
+              " >/proc/1/fd/1 2>/proc/1/fd/2" 
+    
     job_txt += "\n\n"
     return job_txt
 
